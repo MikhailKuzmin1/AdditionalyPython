7845258be7616a7a37154d613c5221340a0aa146
Mikhail <mikyzmin97@gmail.com>
2023-07-04 23:19:31 +0300

    change while

--- table.py
+++ table.py
@@ -2,7 +2,7 @@
 first_number = 2
 second_number = 2
 while first_number <=9:
-    while second_number <= 9:
+    while second_number <= 10:
         print (f'{first_number}*{second_number} = {first_number*second_number}')
         second_number += 1
     first_number += 1
